269424fb6f5806afe084daf2dce5f1000b1293d5
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2023-08-19 10:30:57 +0700

    Authenticate using PostGresql and Prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "./prisma";
+import { compare } from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -17,8 +19,31 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const user = { id: "1", name: "Admin", email: "admin@admin.com" }
-        return user
+        // Static
+        // const user = { id: "1", name: "Admin", email: "admin@admin.com" }
+        // return user
+
+        // Dynamic, checking request to PostgresDB
+        if (!credentials?.email || !credentials.password) {
+          return null
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+
+        if (!user || !(await compare(credentials.password, user.password))) {
+          return null
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: "randomKeyTestDev",
+        }
       },
     }),
   ],
